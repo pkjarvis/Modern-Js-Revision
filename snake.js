46fbc4d1552908bdc1602242cf2b4efb978402d4
Dell <singh.pankaj@jarvis.consulting>
2025-05-02 17:24:06 +0530

    updates

--- snake.js
+++ snake.js
@@ -89,11 +89,245 @@ else console.log("You can have driving license");
   let q=8;
   let answer=z**q;
   console.log(answer);
-
+  console.log("---");
 
 // Nullish coalescing operator,it doesn't work with other operator in conjuction is not seperated by brackets
-let height=5;
-let width=6;
+let height=null;
+let width=null;
 let area=(height ?? 100)*(width ?? 50);
 console.log(area);
+
+
+// Loops, while loop,for loop
+let i=0;
+while(i<3){
+  console.log(i);
   
+  i++;
+}
+
+console.log("---");
+let j=0;
+do{
+  console.log(j);
+  j++;
+}while(j<4)
+
+  console.log("---")
+
+for(let i=0;i<5;i++){
+  console.log(`Rendered ${i}th time`);
+}
+
+console.log("---")
+
+let ap=1+2;
+switch(ap){
+  case 1:
+    console.log("Too small");
+    break;
+  case 2:
+    console.log("Medium");
+    break;
+  case 3:
+    console.log("Big");
+    break;
+  default:
+    console.log("Too big");
+}
+
+// Function , callback function
+  
+let userName="John";
+function showMsg(){
+  let msg="Hello "+userName;
+  return msg;
+}
+
+let v=showMsg();
+console.log(v);
+
+
+const app = (name)=>{
+  console.log("Hello "+`${name}`); 
+}
+
+var pk=app("Jarvis");
+
+let sum=(a,b)=>a+b;
+console.log(sum(4,3));
+
+// String
+let str="Jarvis Technology & Strategy Consulting  ";
+console.log("length of string is:",str.length);
+console.log(str.indexOf('T'));
+console.log(str.lastIndexOf('T'));
+console.log(str.slice(0,5));
+
+console.log(str.substring(0,5));
+
+console.log(str.replace("Jarvis","harvis"));
+console.log(str.trim());
+
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let str1=",Political Consulting"
+console.log(str.charAt(5));
+console.log(str.concat(str,str1));
+
+console.log(str.repeat(6));
+
+// Arrays
+let arr=[1,2,3,4,5];
+arr.push(6);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.shift(); // pop front
+console.log(arr);
+
+arr.unshift(8); // push front
+console.log(arr);
+
+arr.sort();
+console.log("Sorted Array:",arr);
+
+arr.reverse();
+console.log("Reversed Array:",arr);
+
+result=[];
+arr.forEach((item)=>{  // for iterating on each element of array
+  result.push(item*2);
+}
+);
+console.log("Result Array:",result);
+
+let x=arr.find(num=>num%2===0); // it basically returns numbers if present in array based on condition
+console.log(x);
+
+let y=arr.fill(9,0,2);
+console.log(y);
+
+let r=arr.findIndex(num=>num==4);
+console.log(r);
+
+
+const pets=['cats','dogs','bird'];
+console.log(pets.includes('cats'));
+
+let ans=pets.filter((word)=>word.length>3);
+console.log(ans);
+
+
+let val=arr.map((num)=>num*4);
+console.log(val);
+
+
+const numbers=[3,4,2,6,7];
+let value=numbers.reduce((acc,num)=>acc+num,0);
+console.log(value);
+
+// | Method   | Returns      | Purpose                            |
+// | -------- | ------------ | ---------------------------------- |
+// | `filter` | New array    | Keep elements matching a condition |
+// | `map`    | New array    | Transform each element             |
+// | `reduce` | Single value | Accumulate data into one result    |
+
+
+
+let m=new Map();
+m.set(1,"rohan");
+m.set(2,"jatin");
+m.set(3,"saksham");
+
+console.log(m.size);
+console.log(m.has(2));
+console.log(m.get(2));
+
+m.delete(2);
+console.log(m.size);
+
+// iterating over map using keys(),values(),entries()
+
+for( let [key,value] of m){
+  console.log(key,value);
+}
+
+m.clear();
+console.log(m.size);
+
+// set
+
+let s=new Set(['oranges','apples','bananas']);
+s.add('Mangoes');
+
+for(let val of s){
+  console.log(val);
+}
+s.delete('oranges');
+
+
+console.log(s.size);
+console.log(s.has('Mangoes'));
+s.clear();
+
+
+// WeakMap is fundamentally different in this aspect. It doesn’t prevent garbage-collection of key objects, object is passed as key
+let weakmap=new WeakMap();
+let obj={};
+weakmap.set(obj,'ok');
+
+// same method like normal set
+
+
+// Object , we could access using 2 method , either dot method or square bracket
+
+let object = {
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3'
+};
+
+object['key1'] = 'new value1';
+console.log(object['key1']); // Outputs: 'new value1'
+console.log(object.key1);
+
+console.log('key1' in object)
+
+for( let key in object){
+  if(object.hasOwnProperty(key))
+  console.log(key+":"+object[key]);
+}
+
+
+// Error handling
+try {
+  alert( 'try' );
+  if (confirm('Make an error?')) BAD_CODE();
+} catch (err) {
+  alert( 'catch' );
+} finally {
+  alert( 'finally' );
+}
+
+// Promise
+let promise = new Promise(function(resolve, reject) {
+  resolve("done");
+
+  reject(new Error("…")); // ignored
+  setTimeout(() => resolve("…")); // ignored
+});
+
+let promise1 = new Promise(function(resolve, reject) {
+  setTimeout(() => reject(new Error("Whoops!")), 1000);
+});
+
+// reject runs the second function in .then
+promise1.then(
+  result => alert(result), // doesn't run
+  error => alert(error) // shows "Error: Whoops!" after 1 second
+);
